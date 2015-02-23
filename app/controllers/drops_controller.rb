cfc54cbb24a5acbeedae91fc7bc12f6a447b16f1
Christopher Chandler <pherchanplist@gmail.com>
2015-02-22 18:54:03 -0800

    got photos to display within the user controller

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -1,5 +1,6 @@
 class DropsController < ApplicationController
 	def index
+		@photos = Photo.where(recipient_id: current_user.id)
 		@drops = Drop.where(recipient_id: current_user.id)
 	end
 
@@ -11,11 +12,9 @@ class DropsController < ApplicationController
 	  #add the rest of the parameters for our working app LATER
 	  drop = current_user.drops.new(params.require(:drop).permit(:spot, :description, :item, :latitude, :longitude, :landmark, :recipient_id)) 
 	  #which inputs will be allowed in the create controller
-	  drop.recipient_id = 
+	  # drop.recipient_id = 
 	  if drop.save
-
 	    redirect_to drops_path(drop_created: 'true')
-
 	  end
 	end
 
